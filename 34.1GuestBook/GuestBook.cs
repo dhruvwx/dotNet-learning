b699ac36766465e23b5fb0af41cfba072c3065f7
Dhruv Sharma <yesdhruvsharma@gmail.com>
2026-04-17 01:34:23 +0530

    tried to implement class library , methods , auto and full prop loops , instantiation in an already build project with better tools as a new project.

---
+++ 34.1GuestBook/GuestBook.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _GuestBook
+{
+    public class GuestBook
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public string Msg { get; set; }
+
+        public string FullDetails
+        {
+            get
+            {
+                string fullname = $"hi my name is {FirstName} {LastName} i am {Age} years old i wanted to say : {Msg}";
+                return fullname;
+            }
+
+        }
+    }
+
+}
---
+++ 34MiniProject(GuestBook)/Program.cs
@@ -0,0 +1,63 @@
+﻿//capture info about each guest : 1stName, LastName, age ,msg to host , then loop through each guest and priint info.
+
+using _GuestBook;
+//GuestBook people = new GuestBook();
+List<GuestBook> guests = new();
+
+do
+{
+    GuestBook person = new GuestBook();//comes inside loop coz getting ovewritten
+
+
+    Console.Write("enter your first name or type stop to exit:   ");
+    // person.FirstName = Console.ReadLine(); -- wrong coz assigning before checking.
+   string name = Console.ReadLine();
+    if (name.ToLower() == "stop")
+    {
+        break;
+    }
+    person.FirstName = name;
+
+
+    //Console.Write("enter your surname:  ");
+    person.LastName = Helper.GetInfo("Enter your surname");
+
+
+    Console.Write("enter your age:  ");
+    string ageNo = Console.ReadLine();
+    bool valid = int.TryParse(ageNo, out int age);
+    while (valid == false)
+    {
+        Console.Write("enter your correct age:  ");
+        ageNo = Console.ReadLine();
+        valid = int.TryParse(ageNo, out age);
+    }
+    ;
+    person.Age = age;
+
+
+    //Console.Write("Give your message:  ");
+    person.Msg = Helper.GetInfo("Give your Message");
+
+
+
+    guests.Add(person);
+} while (true);
+
+
+
+foreach (GuestBook guest in guests)
+{
+    Console.WriteLine(guest.FullDetails);
+}
+
+public static class Helper
+{
+    public static string GetInfo(string msg)
+    {
+        Console.WriteLine(msg);
+        string info = Console.ReadLine();
+        return info;
+
+    }
+}
