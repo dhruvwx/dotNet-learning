3fade0ef1a65883d33b9a99f6a1df09a15936c2d
Dhruv Sharma <yesdhruvsharma@gmail.com>
2026-04-28 00:01:48 +0530

    Add files via upload

---
+++ 10SQL - Union.sql
@@ -0,0 +1,32 @@
+USE sql_store;
+
+SELECT 
+	customer_id,
+	first_name,
+	points,
+	'SILVER' AS type
+FROM customers
+WHERE points BETWEEN 2000 AND 3000
+
+UNION 
+
+SELECT 
+	customer_id,
+	first_name,
+	points,
+	'GOLD' AS type
+FROM customers
+WHERE points > 3000
+
+
+UNION 
+
+
+SELECT 
+	customer_id,
+	first_name,
+	points,
+	'BRONZE' AS type
+FROM customers
+WHERE points < 2000
+ORDER BY first_name
