c93fa2c263926ab2116714e5d2338c2414f4e08f
Dhruv Sharma <yesdhruvsharma@gmail.com>
2026-04-30 16:57:26 +0530

    Abstract Classes

---
+++ 38AbstractClasses/Program.cs
@@ -0,0 +1,45 @@
+﻿
+
+
+
+List<IRentable> rents = new List<IRentable>();
+Novel novel = new Novel();
+StoryBook storyBook = new StoryBook();
+
+rents.Add(novel);
+rents.Add(storyBook);
+
+
+public interface IRentable
+{
+    void Rent();
+}
+
+public abstract class Book 
+{
+    public string Name { get; set; }
+
+    public int Mrp { get; set; }
+
+}
+
+
+public class Novel : Book , IRentable
+{
+    public string Author { get; set; }
+
+    public string Publisher { get; set; }
+
+    public void Rent()
+    {
+        Console.WriteLine("not rentable");
+    }
+}
+
+public class StoryBook : Book, IRentable
+{
+    public void Rent()
+    {
+        Console.WriteLine("is rentable");
+    }
+}
