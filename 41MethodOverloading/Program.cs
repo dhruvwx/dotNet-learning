8d99c50cf3764cce10c212410b2e6a36d7a18916
Dhruv Sharma <yesdhruvsharma@gmail.com>
2026-05-02 23:54:26 +0530

    OverLoading

---
+++ 41MethodOverloading/Program.cs
@@ -0,0 +1,23 @@
+﻿
+
+EmployeeModel employ = new EmployeeModel("a", "b ", "c");
+Console.WriteLine(employ.LastName);
+
+public class EmployeeModel
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Mail { get; set; }
+
+    public EmployeeModel(string firstname , string lastname , string mail)
+    {
+        FirstName = firstname;
+        LastName = lastname;
+        Mail = mail;
+    }
+    public EmployeeModel(string firstname , string lastname)
+    {
+        FirstName = firstname;
+        LastName = lastname;
+    }
+}
