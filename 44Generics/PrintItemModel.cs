51e06455e481a2813bbfd13b532befb24c3e6ebb
Dhruv Sharma <yesdhruvsharma@gmail.com>
2026-05-04 18:43:14 +0530

    Generics

---
+++ 44Generics/PrintItemModel.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _44Generics
+{
+    public class PrintItemModel
+    {
+        public static void PrintItem<T>(T item)
+        {
+            Console.WriteLine(item.ToString());
+        }
+
+    }
+
+    public class Box<T>
+    {
+        public T Value { get; set; }
+
+        public void Show()
+        {
+            Console.WriteLine(Value);
+        }
+    }
+
+
+    public interface IEntity
+    {
+      int Id { get; set; }
+    }
+
+    public class User : IEntity
+    {
+        public string Name { get; set; }
+        public int Id { get; set ; }
+    }
+
+    public class UseGeneric<T> where T : IEntity
+    {
+        List<T> items = new List<T>();
+
+        public void Add(T item)
+        {
+            items.Add(item);
+        }
+
+        public void PrintId()
+        {
+            foreach(var item in items)
+            {
+                Console.WriteLine(item.Id);
+            }
+        }
+    }
+}
---
+++ 44Generics/Program.cs
@@ -0,0 +1,26 @@
+﻿using _44Generics;
+
+PrintItemModel.PrintItem("hello");
+PrintItemModel.PrintItem(1234);
+PrintItemModel.PrintItem(true);
+
+////////////////////////////////////////////////////////////////
+
+Box<string> strValue = new();
+strValue.Value = "Dhruv";
+strValue.Show();
+
+////////////////////////////////////////////////////////////////
+
+Box<int> intValue = new();
+intValue.Value = 40;
+intValue.Show();
+
+//////////////////////////////////////////////////////////////////
+
+UseGeneric<User> user = new UseGeneric<User>();
+
+user.Add(new User { Id = 1, Name = "Dhruv" });
+user.Add(new User { Id = 2, Name = "Indra" });
+
+user.PrintId();
