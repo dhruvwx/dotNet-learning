dfe1272e2cf087c84f123232e981c264e29dbd55
Dhruv Sharma <yesdhruvsharma@gmail.com>
2026-05-09 00:04:24 +0530

    Unit Testing Mini Project

---
+++ 49.0CalculationTesting-HW/CalculationsClass.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _49._0CalculationTesting_HW
+{
+    public class CalculationsClass
+    {
+        public double Add(double val1 , double val2)
+        {
+            return val1 + val2;
+        }
+        public double Subtract(double val1 , double val2)
+        {
+            return val1 - val2;
+        }
+
+        public double Multiply(double val1, double val2)
+        {
+            return val1 * val2;
+        }
+
+        public double Divide(double val1, double val2)
+        {
+            double output = 0;
+            if (val2 != 0)
+            {
+                output = val1 / val2;
+            }
+            return output;
+        }
+
+    }
+}
---
+++ Calculations.Tests/CalcUnitTest.cs
@@ -0,0 +1,59 @@
+﻿using _49._0CalculationTesting_HW;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculationsLibrary.Tests
+{
+    public class CalcUnitTest
+    {
+        //ARRANGE   ACT  ASSERT
+
+        [Theory]
+        [InlineData(2.3 , 2.7 , 5)]
+        public void AddTest(double val1 , double val2 , double expected)
+        {
+            CalculationsClass sum = new CalculationsClass();
+            double actual = sum.Add(val1, val2);
+            Assert.Equal(expected, actual);
+        }
+
+
+        [Theory]
+        [InlineData(7.7 , 7.7 , 0)]
+        [InlineData(9 , -2 , 11)]
+        public void SubtractTest(double val1, double val2, double expected)
+        {
+            CalculationsClass subtract = new CalculationsClass();
+            double actual = subtract.Subtract(val1, val2);
+            Assert.Equal(expected, actual);
+        }
+
+
+        [Theory]
+        [InlineData(120 , -10 ,-1200)]
+        [InlineData(120 , 0 ,0)]
+        public void MultiplyTest(double val1, double val2, double expected)
+        {
+            CalculationsClass multiply = new CalculationsClass();
+            double actual = multiply.Multiply(val1, val2);
+            Assert.Equal(expected, actual);
+        }
+
+
+
+        [Theory]
+        [InlineData(20 , 10 , 2)]
+        [InlineData(-20 , 10 , -2)]
+        [InlineData(20 , 0 , 0)]
+        public void DivideTest(double val1, double val2, double expected)
+        {
+            CalculationsClass divide = new CalculationsClass();
+            double actual = divide.Divide(val1, val2);
+            Assert.Equal(expected, actual);
+        }
+
+    }
+}
