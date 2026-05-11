87fab4129f8932fbcf1715f3527e05c0a1eb713d
Dhruv Sharma <yesdhruvsharma@gmail.com>
2026-05-11 22:28:59 +0530

    Lambda and Linq statements.

---
+++ 0LinqLamda/Program.cs
@@ -0,0 +1,91 @@
+﻿using _0LinqLamda;
+using System.Linq;
+using System.Net.Mail;
+
+
+
+
+LambdaTests();
+LinqTests();
+
+
+static void LambdaTests() //--Uses Arrow Functions.
+{
+    var data = SampleData.GetContactsData();
+
+    /* .WHERE returns bool
+    =================================================================*/
+    //var results = data.Where(x => x.Addresses.Count > 1);
+    //foreach(var item in results)
+    //{
+    //    Console.WriteLine($"{item.FirstName} {item.LastName}");
+    //}
+
+
+    /*  .SELECT returns string
+    =================================================================*/
+    //var results = data.Select(x => x.FirstName);
+    //foreach (var item in results)
+    //{
+    //    Console.WriteLine(item);
+    //}
+
+
+    /*   .TAKE()  & .SKIP
+    =================================================================*/
+    //var results = data.Skip(2).Take(3);
+    //foreach (var item in results)
+    //{
+    //    Console.WriteLine(item.FirstName);
+    //}
+
+
+
+    /*   .OrderBY & .OrderByDescending
+    =================================================================*/
+    //var results = data.OrderByDescending(x => x.Id);  
+    //foreach (var item in results)
+    //{
+    //    Console.WriteLine(item.FirstName +" "+ item.Id);
+    //}
+}
+
+
+static void LinqTests()
+{
+    var contactData = SampleData.GetContactsData();
+    var addressData = SampleData.GetAddressModel();
+
+    //var result = ( //sql like statement
+    //                from c in contactData
+    //                where c.FirstName == "Dhruv" || c.FirstName == "Raja"
+    //                select c
+    //             );
+    //foreach (var item in result)
+    //{
+    //    Console.WriteLine(item.LastName);
+    //}
+
+
+
+    //var result = (from c in contactData
+    //              join a in addressData
+    //              on c.Id equals a.Id
+    //              select new { c.FirstName, a.City });
+    //foreach (var item in result)
+    //{
+    //    Console.WriteLine(item.FirstName + " Lives in " + item.City);
+    //}
+
+
+
+    var result = (from c in contactData
+                  select new { c.FirstName, c.LastName, Address = addressData.Where(x => c.Addresses.Contains(x.Id)) }
+                  );
+    foreach (var item in result)
+    {
+        Console.WriteLine($"{item.FirstName} {item.LastName} {item.Address.Count()}");
+    }
+}
+
+
---
+++ 0LinqLamda/SampleData.cs
@@ -0,0 +1,39 @@
+﻿using _0LinqLamda.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0LinqLamda
+{
+    public static class SampleData
+    {
+        public static List<ContactModel> GetContactsData()
+        {
+            List<ContactModel> output = new()
+            {
+                new ContactModel { Id = 1, FirstName = "Dhruv", LastName = "Sharma" , Addresses = new List<int> {1,2,3}},
+                new ContactModel { Id = 2, FirstName = "Indra", LastName = "Sharma", Addresses = new List<int> { 2, 3 } },
+                new ContactModel { Id = 3, FirstName = "Raja", LastName = "Rana", Addresses = new List<int> { 3 } },
+                new ContactModel { Id = 4, FirstName = "Rani", LastName = "Thakur", Addresses = new List<int> { 1 } },
+                new ContactModel { Id = 5, FirstName = "Jatt", LastName = "Singh", Addresses = new List<int> { 2, 3 } }
+            };
+            return output;
+        }
+
+        public static List<AddressModel> GetAddressModel()
+        {
+            List<AddressModel> output = new()
+            {
+            new AddressModel { Id = 1, ContactId = 1, City = "Shimla", State = "Himachal" },
+            new AddressModel { Id = 2, ContactId = 3, City = "Shimla", State = "Himachal" },
+            new AddressModel { Id = 3, ContactId = 5, City = "Shimla", State = "Himachal" },
+            new AddressModel { Id = 4, ContactId = 5, City = "Mohali", State = "Punjab" },
+            new AddressModel { Id = 5, ContactId = 5, City = "Gurgao", State = "Haryana" },
+            };
+            return output;
+            
+        }
+    }
+}
